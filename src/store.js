d2ee0ae1bcc21a4677283799e9c9e9dc99a2b042
Chanuth10 <it20074418@my.sliit.lk>
2023-07-24 14:07:14 +0530

    add all reducer section and combine as  encapsule

---
+++ src/store.js
@@ -0,0 +1,95 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productsReducer,
+  productDetailsReducer,
+  deleteProductReducer,
+  newProductReducer,
+} from "./reducers/ProductReducer";
+import { cartReducer } from "./reducers/CartReducer";
+import { favouriteReducer } from "./reducers/FavouriteReducer";
+import {
+  allUsersReducer,
+  forgotPasswordReducer,
+  profileReducer,
+  userDetailsReducer,
+  userReducer,
+} from "./reducers/userReducer";
+
+import {
+  allOrdersReducer,
+  myOrdersReducer,
+  newOrderReducer,
+  orderDetailsReducer,
+  orderReducer,
+} from "./reducers/OrderReducer";
+
+
+
+import {
+  categoryReducer,
+  deleteCategoryReducer,
+  newCategoryReducer,
+  categoryDetailsReducer,
+} from "./reducers/CategoryReducer";
+import {
+  brandReducer,
+  deleteBrandReducer,
+  newBrandReducer,
+  brandDetailsReducer,
+} from "./reducers/BrandReducer";
+const reducer = combineReducers({
+  products: productsReducer,
+  categories: categoryReducer,
+  productDetails: productDetailsReducer,
+  categoryDetails: categoryDetailsReducer,
+  allUsers: allUsersReducer,
+  forgotPassword: forgotPasswordReducer,
+  deleteProduct: deleteProductReducer,
+  deleteCategory: deleteCategoryReducer,
+  userDetails: userDetailsReducer,
+  profile: profileReducer,
+  createProduct: newProductReducer,
+  createCategory: newCategoryReducer,
+  user: userReducer,
+  cart: cartReducer,
+  favourite: favouriteReducer,
+  AllOrders: allOrdersReducer,
+  favourite: favouriteReducer,
+  order: newOrderReducer,
+  myOrder: myOrdersReducer,
+  myOrderDetails: orderDetailsReducer,
+  deleteOrder: orderReducer,
+  brands: brandReducer,
+  brandDetails: brandDetailsReducer,
+  deleteBrand: deleteBrandReducer,
+  createBrand: newBrandReducer,
+});
+
+let initialState = {
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+
+    shippingInfo: localStorage.getItem("shippingInfo")
+      ? JSON.parse(localStorage.getItem("shippingInfo"))
+      : {},
+  },
+  favourite: {
+    favouriteItems: localStorage.getItem("favouriteItems")
+      ? JSON.parse(localStorage.getItem("favouriteItems"))
+      : [],
+  },
+};
+
+const middleWare = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleWare))
+);
+
+export default store;
