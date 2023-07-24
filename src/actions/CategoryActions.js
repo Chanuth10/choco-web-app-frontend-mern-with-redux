c8678efae21e17b1503c18595071eaad388c9b69
Chanuth10 <it20074418@my.sliit.lk>
2023-07-24 14:01:55 +0530

    add category section action from backend apicall across axios call

---
+++ src/actions/CategoryActions.js
@@ -0,0 +1,136 @@
+import axios from "axios";
+import {
+  ADMIN_CATEGORY_FAIL,
+  ADMIN_CATEGORY_REQUEST,
+  ADMIN_CATEGORY_SUCCESS,
+  CLEAR_ERRORS,
+  DELETE_CATEGORY_FAIL,
+  DELETE_CATEGORY_REQUEST,
+  DELETE_CATEGORY_SUCCESS,
+  NEW_CATEGORY_FAIL,
+  NEW_CATEGORY_REQUEST,
+  NEW_CATEGORY_SUCCESS,
+  UPDATE_CATEGORY_FAIL,
+  UPDATE_CATEGORY_REQUEST,
+  UPDATE_CATEGORY_SUCCESS,
+  CATEGORY_DETAILS_FAIL,
+  CATEGORY_DETAILS_REQUEST,
+  CATEGORY_DETAILS_SUCCESS,
+} from "../constans/CategoryConstans";
+
+// Create Product --------Admin
+export const createCategory = (categoryData) => async (dispatch) => {
+  try {
+    dispatch({ type: NEW_CATEGORY_REQUEST });
+    console.log(categoryData);
+    const config = {
+      headers: { "Content-Type": "application/json" },
+    };
+
+    const { data } = await axios.post(
+      `https://choco-e-app.onrender.com/api/v2/category/new`,
+      categoryData,
+      config
+    );
+    console.log(data);
+    dispatch({
+      type: NEW_CATEGORY_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: NEW_CATEGORY_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// Get Admin Products -----Admin
+export const getAdminCategory = () => async (dispatch) => {
+  try {
+    dispatch({ type: ADMIN_CATEGORY_REQUEST });
+
+    const { data } = await axios.get("https://choco-e-app.onrender.com/api/v2/admin/categories");
+
+    dispatch({
+      type: ADMIN_CATEGORY_SUCCESS,
+      payload: data.categories,
+    });
+  } catch (error) {
+    dispatch({
+      type: ADMIN_CATEGORY_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// Delete Product ------Admin
+export const deleteCategory = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: DELETE_CATEGORY_REQUEST });
+
+    const { data } = await axios.delete(`https://choco-e-app.onrender.com/api/v2/category/${id}`);
+
+    dispatch({
+      type: DELETE_CATEGORY_SUCCESS,
+      payload: data.success,
+    });
+  } catch (error) {
+    dispatch({
+      type: DELETE_CATEGORY_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// Update Product
+export const updateCategory = (id, categoryData) => async (dispatch) => {
+  try {
+    dispatch({ type: UPDATE_CATEGORY_REQUEST });
+
+    const config = {
+      headers: { "Content-Type": "application/json" },
+    };
+
+    const { data } = await axios.put(
+      `https://choco-e-app.onrender.com/api/v2/category/${id}`,
+      categoryData,
+      config
+    );
+
+    dispatch({
+      type: UPDATE_CATEGORY_SUCCESS,
+      payload: data.success,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_CATEGORY_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+//   Clearing errors
+export const clearErrors = () => async (dispatch) => {
+  dispatch({
+    type: CLEAR_ERRORS,
+  });
+};
+
+// Get All Products Details
+export const getCategoryDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: CATEGORY_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`https://choco-e-app.onrender.com/api/v2/category/${id}`);
+    dispatch({
+      type: CATEGORY_DETAILS_SUCCESS,
+      payload: data.category,
+    });
+  } catch (error) {
+    dispatch({
+      type: CATEGORY_DETAILS_FAIL,
+      payload: error.response.message,
+    });
+  }
+};
